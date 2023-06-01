42a970dba1a582947ad828997bb5fa7013ec6afe
klownfish <github@clownfish.xyz>
2023-06-01 13:15:37 +0200

    Improve event counter prints

--- recorder/event_counter.cpp
+++ recorder/event_counter.cpp
@@ -1,64 +1,88 @@
 #include <stdio.h>
 #include "protocol.h"
 
+#define MAX(a, b) a < b ? b : a
+#define MIN(a, b) a < b ? a : b
+
 int main(int argc, char** argv) {
     if (argc != 2) {
         return -1;
     }
     FILE *f = fopen(argv[1], "r");
 
+
+    uint32_t pc_timestamp = 0;
+
+    uint32_t min_optitrack_timestamp_delta = ~0;
+    uint32_t max_optitrack_timestamp_delta = 0;
+    uint32_t min_event_timestamp_delta = ~0;
+    uint32_t max_event_timestamp_delta = 0;
+
+    uint64_t num_events;
+    uint64_t num_tracks;
+    uint64_t num_timestamps;
+
+    uint8_t optitrack_ids[255] = {0};
+
+    uint32_t last_print = 0;
+
+    uint32_t first_print = 0;
+
     while (!feof(f)) {
         int id = fgetc(f);
-        // printf("id: %d\n", id);
         if (id == CAMERA_HEADER) {
-            static uint64_t t = 0;
-            static uint64_t events = 0;
             struct camera_header header;
             fread(&header, 1, sizeof(header), f);
             for (int i = 0; i < header.num_events; i++) {
                 struct camera_event entry;
                 fread(&entry, 1, sizeof(entry), f);
-                if (entry.t / 1000000 != t / 1000000) {
-                    t = entry.t;
-                    printf("Events %lld:%lld\n", entry.t / 1000000, events);
-                    events = 0;
-                } else {
-                    events++;
-                }
+                uint32_t ts_delta = entry.t - pc_timestamp;
+                min_event_timestamp_delta = MIN(min_event_timestamp_delta, ts_delta);
+                max_event_timestamp_delta = MAX(max_event_timestamp_delta, ts_delta);
+                num_events++;
             }
         }
         else if (id == TIMESTAMP_HEADER) {
-            static uint32_t prev;
-            static int i = 0;
             struct timestamp_header header;
             fread(&header, 1, sizeof(header), f);
-            i += 1;
-            if (header.t - prev > 1000000) {
-                printf("timestamp %lld: %lld\n", header.t / 1000000, i);
-                prev = header.t;
-                i = 0;
-            }
+            pc_timestamp = header.t;
+            num_timestamps++;
         }
         else if (id == OPTITRACK_HEADER) {
-            static uint32_t prev;
-            static int i;
-            static uint8_t ids[255] = {0};
             struct optitrack_header header;
             fread(&header, 1, sizeof(header), f);
-            i++;
-            ids[header.object_id] = 1;
-            if (header.t - prev > 1000000) {
-                int sum = 0;
-                for (int j = 0; j < 256; j++) {
-                    sum += ids[j];
-                }
-                printf("tracking %d objects at %fHz\n", sum,  (float) i / sum);
-                i = 0;
-                prev = header.t;
-            }
+            optitrack_ids[header.object_id] = 1;
+            uint32_t ts_delta = header.t - pc_timestamp;
+            min_optitrack_timestamp_delta = MIN(min_optitrack_timestamp_delta, ts_delta);
+            max_optitrack_timestamp_delta = MAX(max_optitrack_timestamp_delta, ts_delta);
+            num_tracks++;
         }
         else {
             return -2;
+        }
+
+        if (pc_timestamp - last_print > 1e6) {
+            int sum = 0;
+            for (int j = 0; j < 255; j++) {
+                sum += optitrack_ids[j];
+            }
+            last_print = pc_timestamp;
+            printf("*******%llu*******\n", pc_timestamp / 1e6);
+            printf("events: %fM\n", num_events / 1e6);
+            printf("events delta: %llu\n", max_event_timestamp_delta - min_event_timestamp_delta);
+            if (sum != 0) {
+                printf("optitrack: %llu objects at %dHz\n", sum,  num_tracks / sum);
+                printf("optitrack delta: %llu\n", max_optitrack_timestamp_delta - min_optitrack_timestamp_delta);
+            }
+            printf("timestamps: %llu\n", num_timestamps);
+
+            min_optitrack_timestamp_delta = ~0;
+            max_optitrack_timestamp_delta = 0;
+            min_event_timestamp_delta = ~0;
+            max_event_timestamp_delta = 0;
+            num_events = 0;
+            num_timestamps = 0;
+            num_tracks = 0;
         }
     }
 }
